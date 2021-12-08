da49d7b21e0f01c84d3cc5fd81eaf1b659a172b7
Marcus Sena <maarvins.19@gmail.com>
2021-12-07 21:39:15 -0300

    chore: fonts defined

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -38,6 +38,15 @@ body{
   -webkit-font-smoothing:antialiased; /*Hacker para deixar fonts mais legíveis em navegadores além do chrome */
 }
 
+body, textArea, input, button{
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+}
+
+h1,h2,h3,h4,h5,h6, strong {
+  font-weight: 600;
+}
+
 button{
   cursor:pointer;
 }
@@ -46,4 +55,5 @@ button{
   opacity: 0.6;
   cursor: not-allowed;
 }
+
 `
