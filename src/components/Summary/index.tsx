ee5c9d8ec4546605cece296b7f9a2a397d15b461
Marcus Sena <maarvins.19@gmail.com>
2021-12-12 17:41:46 -0300

    feat: add a new transaction on a dashboard

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,16 @@
-import {Container} from "./styles"
-
 import incomeImg from "../../assets/income.svg"
 import outcomeImg from "../../assets/outcome.svg"
 import totalImg from "../../assets/total.svg"
+import {useContext} from "react"
+import {TransactionsContext} from "../../TransactionsContext"
+
+import {Container} from "./styles"
 
 export function Summary() {
+  const {transactions} = useContext(TransactionsContext)
+
+  console.log(transactions)
+
   return (
     <Container>
       <div>
