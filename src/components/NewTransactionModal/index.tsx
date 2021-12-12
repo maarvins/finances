cb3a346dcc4d920d3d0668fd1582919a37316c7b
Marcus Sena <maarvins.19@gmail.com>
2021-12-12 18:36:38 -0300

    feat: create my fisrt hook and apply on this app

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import {FormEvent, useState, useContext} from "react"
 import Modal from "react-modal"
-import {TransactionsContext} from "../../TransactionsContext"
+import {useTransactions} from "../../hooks/useTransactions"
 
 import ImgIncome from "../../assets/income.svg"
 import ImgClose from "../../assets/close.svg"
@@ -17,7 +17,7 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose
 }: NewTransactionModalProps) {
-  const {createTransaction} = useContext(TransactionsContext)
+  const {createTransaction} = useTransactions()
   const [title, setTitle] = useState("")
   const [amount, setAmount] = useState(0)
   const [category, setCategory] = useState("")
--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,5 +1,5 @@
-import {createContext, useEffect, useState, ReactNode} from "react"
-import {api} from "./services/api"
+import {createContext, useEffect, useState, ReactNode, useContext} from "react"
+import {api} from "../services/api"
 
 interface Transaction {
   id: number
@@ -31,7 +31,7 @@ interface TransactionsContextData {
   createTransaction: (transaction: TransactionInput) => Promise<void>
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
 )
 
@@ -58,4 +58,9 @@ export function TransactionProvider({children}: TransactionsProviderProps) {
       {children}
     </TransactionsContext.Provider>
   )
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext)
+  return context
 }
